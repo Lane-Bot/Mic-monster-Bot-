3a4b52233be7566cab1cbfa481418b0a6429e396
Pannawat_Lane <78841807+Lane-Bot@users.noreply.github.com>
2023-07-19 10:57:18 +0700

    Change name to Micmoster

--- MicMonster.py
+++ MicMonster.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
 import tkinter.filedialog as filedialog
-import tkinter.messagebox as messagebox
 import os
 import docx
 import PyPDF2
@@ -14,6 +15,8 @@ from selenium.common.exceptions import NoSuchElementException
 driver = None  # Global variable to hold the browser instance
 processed_files = set()  # Set to store the names of processed files
 save_path = ""  # Global variable to store the save path
+current_file = ""  # Global variable to store the name of the current file being processed
+cancel_processing = False  # Flag to indicate if the file processing should be canceled
 
 def open_mic_monster():
     global driver
@@ -29,13 +32,23 @@ def open_mic_monster():
         return
 
 def insert_text_from_file():
+    global cancel_processing
     folder_path = filedialog.askdirectory()
     if folder_path:
         files = get_files_in_folder(folder_path)
-        for file_path in files:
+        progress_bar["maximum"] = len(files)
+        cancel_processing = False
+        for i, file_path in enumerate(files):
+            if cancel_processing:
+                break
             if file_path not in processed_files:
+                current_file_label.config(text=f"Processing: {os.path.basename(file_path)}")
                 process_file(file_path)
                 processed_files.add(file_path)
+                progress_bar["value"] = i + 1
+                root.update()  # Update the GUI
+        progress_bar["value"] = 0
+        current_file_label.config(text="Processing: None")
     else:
         messagebox.showwarning("No Folder Selected", "No folder was selected.")
 
@@ -47,6 +60,9 @@ def get_files_in_folder(folder_path):
     return files
 
 def process_file(file_path):
+    global cancel_processing
+    if cancel_processing:
+        return
     extension = os.path.splitext(file_path)[1].lower()
     try:
         file_name = os.path.splitext(os.path.basename(file_path))[0]  # Extract the file name without extension
@@ -56,7 +72,6 @@ def process_file(file_path):
                 text = file.read()
                 text_box.delete("1.0", tk.END)  # Clear the existing text
                 text_box.insert(tk.END, text)  # Insert the file contents into the text box
-                messagebox.showinfo("File Read", "Text file contents have been inserted into the text box.")
                 insert_text_into_website(text, file_name)  # Insert the text into the website with the file name
         elif extension == ".docx":
             doc = docx.Document(file_path)
@@ -64,7 +79,6 @@ def process_file(file_path):
             text = "\n".join(paragraphs)
             text_box.delete("1.0", tk.END)  # Clear the existing text
             text_box.insert(tk.END, text)  # Insert the file contents into the text box
-            messagebox.showinfo("File Read", "DOCX file contents have been inserted into the text box.")
             insert_text_into_website(text, file_name)  # Insert the text into the website with the file name
         elif extension == ".pdf":
             with open(file_path, "rb") as file:
@@ -73,11 +87,13 @@ def process_file(file_path):
                 text = "\n".join(pages)
                 text_box.delete("1.0", tk.END)  # Clear the existing text
                 text_box.insert(tk.END, text)  # Insert the file contents into the text box
-                messagebox.showinfo("File Read", "PDF file contents have been inserted into the text box.")
                 insert_text_into_website(text, file_name)  # Insert the text into the website with the file name
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
+def cancel_process():
+    global cancel_processing
+    cancel_processing = True
 
 def copy_text():
     text = text_box.get("1.0", tk.END)
@@ -133,38 +149,6 @@ def insert_text_into_website(text, file_name):
 
             # Refresh the page
             driver.refresh()
-
-            messagebox.showinfo("Text Inserted", "The text has been inserted into the website.")
-        except NoSuchElementException as e:
-            print("Element Not Found:", str(e))
-            messagebox.showerror("Error", "Failed to locate elements on the website.")
-        except Exception as e:
-            print("Exception:", str(e))
-            messagebox.showerror("Error", "Failed to insert text into the website.")
-
-
-            # Click on the "Generate" button
-            generate_button.click()
-
-            # Wait for the page to refresh
-            page_refresh_delay = int(page_refresh_entry.get())
-            time.sleep(page_refresh_delay)  # Adjust the delay as needed
-
-            # Find and click the download button
-            download_button = driver.find_element(By.XPATH, "//button[contains(., 'Download')]")
-            download_button.click()
-
-            # Wait for the download to complete
-            download_delay = int(download_delay_entry.get())
-            time.sleep(download_delay)  # Adjust the delay as needed
-
-            # Rename the downloaded file
-            rename_file(text)
-
-            # Refresh the page
-            driver.refresh()
-
-            messagebox.showinfo("Text Inserted", "The text has been inserted into the website.")
         except NoSuchElementException as e:
             print("Element Not Found:", str(e))
             messagebox.showerror("Error", "Failed to locate elements on the website.")
@@ -193,8 +177,6 @@ def rename_file(file_path):
     except Exception as e:
         print("Exception:", str(e))
 
-
-
 def select_save_path():
     global save_path
     save_path = filedialog.askdirectory()
@@ -204,7 +186,7 @@ def select_save_path():
 # Create the main window
 root = tk.Tk()
 root.title("Mic Monster")
-root.geometry("1280x720")
+root.geometry("1280x820")
 
 # Create the input text box
 text_box = tk.Text(root, height=30)  # Adjust the height as per your requirement
@@ -222,6 +204,26 @@ insert_button.pack()
 copy_button = tk.Button(root, text="Copy Text", command=copy_text)
 copy_button.pack()
 
+# Create a frame for the progress bar
+progress_frame = tk.Frame(root)
+progress_frame.pack()
+
+# Create the progress bar
+progress_bar = ttk.Progressbar(progress_frame, mode="determinate")
+progress_bar.pack(fill=tk.X, padx=10, pady=10)
+
+# Create a frame for the current file label and cancel button
+file_frame = tk.Frame(root)
+file_frame.pack()
+
+# Create the label for the current file
+current_file_label = tk.Label(file_frame, text="Processing: None")
+current_file_label.pack(side=tk.LEFT)
+
+# Create the cancel button
+cancel_button = tk.Button(file_frame, text="Cancel", command=cancel_process)
+cancel_button.pack(side=tk.LEFT)
+
 # Create a frame for the sleep durations
 sleep_frame = tk.Frame(root)
 sleep_frame.pack()
@@ -238,7 +240,7 @@ download_delay_label = tk.Label(sleep_frame, text="Download Delay (seconds):")
 download_delay_label.pack(side=tk.LEFT)
 download_delay_entry = tk.Entry(sleep_frame)
 download_delay_entry.pack(side=tk.LEFT)
-download_delay_entry.insert(tk.END, "10")  # Set a default value
+download_delay_entry.insert(tk.END, "20")  # Set a default value
 
 # Create a frame for the save path section
 save_path_frame = tk.Frame(root)
